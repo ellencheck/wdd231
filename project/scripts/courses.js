11b1db489f2290329021821e7474665dc949a6ee
ellencheck <ellencheck16@gmail.com>
2025-10-04 11:03:05 -0300

    Update courses.js

--- project/scripts/courses.js
+++ project/scripts/courses.js
@@ -15,6 +15,7 @@ fetch("data/courses.json")
         const div = document.createElement("div");
         div.className = "course";
 
+        // Используем span с классом для маленького эмодзи
         div.innerHTML = `
           <h3>
             <span class="flag-emoji">${course.flag}</span>
