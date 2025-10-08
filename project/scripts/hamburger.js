4786bcb8ff58ffa3803a416b258a768379645d3c
ellencheck <ellencheck16@gmail.com>
2025-10-07 23:36:59 -0300

    Update hamburger.js

--- project/scripts/hamburger.js
+++ project/scripts/hamburger.js
@@ -1,12 +1,12 @@
 // scripts/hamburger.js
 
-// Находим кнопку и меню
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('nav ul');
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navMenu = document.querySelector('nav ul');
 
-if (hamburger && navMenu) {
-  // Когда нажимаем на кнопку — переключаем меню
-  hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('show-menu');
-  });
-}
+  if (hamburger && navMenu) {
+    hamburger.addEventListener('click', () => {
+      navMenu.classList.toggle('show-menu');
+    });
+  }
+});
